2c03ff63d0141e8b5bc13879446dda08786c7edc
Lefeuve Vincent <vincent.lefeuve@student-cs.fr>
2023-11-16 15:18:56 +0100

    Added banner to view

--- src/model.py
+++ src/model.py
@@ -2,16 +2,18 @@ import pandas as pd
 
 
 class Model:
-    def __init__(self, dataset):
+    def __init__(self, dataset) -> None:
         self.data = pd.read_csv(dataset)
         useless=["userid", "tweetid", "following", "totaltweets", "original_tweet_id", "original_tweet_user_id", "original_tweet_username", "in_reply_to_status_id", "in_reply_to_user_id", "in_reply_to_screen_name", "is_quote_status", "quoted_status_id", "quoted_status_userid", "quoted_status_username", "extractedts", "coordinates"]
         for m in useless:
             if m in self.data.columns:
                 self.data.drop(m, inplace=True, axis=1)
-        print(self.data.columns)
+        print(self.data['language'].unique())
         
     
-
+    def getData(self) -> None:
+        return self.data
+    
 if __name__=='__main__':
     M=Model('../data/Tweets Ukraine/0402_UkraineCombinedTweetsDeduped.csv')
     
