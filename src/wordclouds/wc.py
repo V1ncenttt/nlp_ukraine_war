96476664ee8b089981a243336346e8b1ee16437d
Nicolas Brandel <nicolas.brandel@student-cs.fr>
2023-11-20 10:52:16 +0100

    Wordcloud avec tous les mots

--- src/wordclouds/wc.py
+++ src/wordclouds/wc.py
@@ -1,7 +1,6 @@
-from wordcloud import WordCloud
-from wordcloud import STOPWORDS
-from wordcloud import ImageColorGenerator
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 from PIL import Image
 import numpy as np
 import pandas as pd
@@ -9,29 +8,36 @@ import spacy
 from spacy.lang.es.stop_words import STOP_WORDS as es_stopwords
 
 # Importation du dataframe panda
-data = pd.read_csv("../data/0908_UkraineCombinedTweetsDeduped.csv")
-data =data.head(1000)
+data = pd.read_csv("../../data/0908_UkraineCombinedTweetsDeduped.csv")
 
-if __name__=="__main__":
-    
+if __name__ == "__main__":
     # Pour retirer les mots "parasite"
     stopwords = set(STOPWORDS)
-    stopwords.add('https')
-    stopwords.add('t')
-    stopwords.add('co')
-    stopwords |= set(es_stopwords)
+    stopwords.add("https")
+    stopwords.add("t")
+    stopwords.add("co")
+    stopwords.add("will")
+    stopwords |= set(es_stopwords)  # Stopwords espagnols
 
-    text = " ".join(i for i in data['text'])
+    # Creation d'une chaine de caractere contenant tous les tweets
+    text = " ".join(i for i in data["text"])
 
-    colors = ImageColorGenerator(np.array(Image.open('map.png')))
+    # Couleurs du wordcloud
+    custom_colors = ["royalblue", "gold"]
+    custom_cmap = ListedColormap(custom_colors)
 
-    # Création du WordCloud
-    wordcloud = WordCloud(stopwords=stopwords, normalize_plurals=True, max_font_size=40,
-                          background_color='white', mask=np.array(Image.open('map.png')), 
-                          color_func=colors, contour_color='black', contour_width=2).generate(text)
+    # Création du wordcloud
+    wordcloud = WordCloud(
+        stopwords=stopwords,
+        height=600,
+        width=800,
+        normalize_plurals=True,
+        colormap=custom_cmap,
+        background_color="white",
+    ).generate(text)
 
-    # Affichage du WordCloud
+    # Affichage du wordcloud
     plt.figure(figsize=(10, 5))
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis('off')
+    plt.imshow(wordcloud, interpolation="bilinear")
+    plt.axis("off")
     plt.show()
