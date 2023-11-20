02b7b05b20aa04fa70546e4b835f35601997f931
Gauthier Riquier <gauthier.riquier@student-cs.fr>
2023-11-20 10:19:38 +0100

    documentation de wordcloud_hashtags

--- src/wordcloud_hashtags.py
+++ src/wordcloud_hashtags.py
@@ -5,29 +5,41 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from src.model import Model
 import nltk
-
 nltk.download('punkt')
 nltk.download('stopwords')
 
-#Chargement de la base de donnée à partir de la classe model
+# Loading the database from the 'Model' class
 M=Model('data/Tweets Ukraine/0402_UkraineCombinedTweetsDeduped.csv')
 df=M.getData()
-#sélection d'un échantillon aléatoire de 50 000 tweets
-#df=df.sample(n=50000)
-df=df.head(4)
+# Selecting a random sample of 50,000 tweets
+df=df.sample(n=50)
+#df=df.head(4)
 def generateWordcloud(df):
+    """
+    Generates and displays a WordCloud based on hashtags in the DataFrame.
+
+    Args:
+        df (pandas.DataFrame): The DataFrame containing tweet data.
+
+    Raises:
+        ValueError: If the 'hashtags' column is not present in the DataFrame.
+    """
+    
+# Check if the 'hashtags' column exists in the DataFrame
     if 'hashtags' not in df.columns:
         raise ValueError("La colonne 'hashtags' n'existe pas dans le DataFrame.")
 
-    # Concaténation de tous les hashtags dans une seule chaîne de caractères
+# Concatenate all hashtags into a single string
     all_hashtags = ' '.join(df['hashtags'].sum())
 
-    # Création du WordCloud
+# Create the WordCloud
     wordcloud = WordCloud(width=800, height=400, background_color='white').generate(all_hashtags)
 
-    # Affichage du WordCloud à l'aide de matplotlib
+# Display the WordCloud using matplotlib
     plt.figure(figsize=(10, 5))
     plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis('off')
     plt.show()
-#generateWordcloud(df)
+    
+# Generating and displaying the WordCloud
+generateWordcloud(df)
