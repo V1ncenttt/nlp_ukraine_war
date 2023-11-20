5755f0fc7accfc5dfaa1ea58879da13b3e2bbc3f
Nicolas Brandel <nicolas.brandel@student-cs.fr>
2023-11-20 11:21:30 +0100

    Wordcloud avec la bonne forme

--- src/wordclouds/wc.py
+++ src/wordclouds/wc.py
@@ -7,11 +7,11 @@ import pandas as pd
 import spacy
 from spacy.lang.es.stop_words import STOP_WORDS as es_stopwords
 
-# Importation du dataframe panda
+# Panda dataframe importation
 data = pd.read_csv("../../data/0908_UkraineCombinedTweetsDeduped.csv")
 
 if __name__ == "__main__":
-    # Pour retirer les mots "parasite"
+    # Creating stopwords list
     stopwords = set(STOPWORDS)
     stopwords.add("https")
     stopwords.add("t")
@@ -19,24 +19,32 @@ if __name__ == "__main__":
     stopwords.add("will")
     stopwords |= set(es_stopwords)  # Stopwords espagnols
 
-    # Creation d'une chaine de caractere contenant tous les tweets
+    # Creating a string containing all the tweets
     text = " ".join(i for i in data["text"])
 
-    # Couleurs du wordcloud
-    custom_colors = ["royalblue", "gold"]
+    # Load the Ukrainian flag image as a mask
+    mask = Image.open('drapeau-ukraine.png').convert('RGBA')
+    white_bg = Image.new('RGBA', mask.size, 'WHITE')
+    white_bg.paste(mask, (0,0), mask)
+    white_bg.convert('RGB')
+    ukraine_shape_mask = np.array(white_bg)
+
+    # Colors of the wordcloud
+    custom_colors = ['royalblue', 'gold']
     custom_cmap = ListedColormap(custom_colors)
 
-    # Création du wordcloud
+    # Creating wordcloud
     wordcloud = WordCloud(
         stopwords=stopwords,
         height=400,
         width=800,
+        mask=ukraine_shape_mask,
         normalize_plurals=True,
         colormap=custom_cmap,
         background_color="white",
     ).generate(text)
 
-    # Affichage du wordcloud
+    # Displaying wordcloud
     plt.figure(figsize=(10, 5))
     plt.imshow(wordcloud, interpolation="bilinear")
     plt.axis("off")
