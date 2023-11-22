3f89c586fddbac202c7452fa651d011bf2fbbc8e
Nicolas Brandel <nicolas.brandel@student-cs.fr>
2023-11-22 11:19:22 +0100

    wordcloud in controller

--- src/controller.py
+++ src/controller.py
@@ -1,7 +1,8 @@
 from src.model import Model
 import pandas as pd
 import matplotlib.pyplot as plt
-from wordcloud import WordCloud, ImageColorGenerator
+from matplotlib.colors import ListedColormap
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from src.model import Model
@@ -10,6 +11,8 @@ from io import BytesIO
 import numpy as np
 import nltk
 import base64
+import spacy
+from spacy.lang.es.stop_words import STOP_WORDS as es_stopwords
 
 
 
@@ -94,6 +97,54 @@ class Controller:
         img_binary = img_buffer.getvalue()
         img_src = f"data:image/png;base64,{base64.b64encode(img_binary).decode()}"
         return img_src
+    
+    def generate_classical_wordcloud(self,date):
+
+        df = self.models[date].data
+        # Creating stopwords list
+        stopwords = set(STOPWORDS)
+        stopwords.add("https")
+        stopwords.add("t")
+        stopwords.add("co")
+        stopwords.add("will")
+        stopwords |= set(es_stopwords)  # Stopwords espagnols
+
+        # Creating a string containing all the tweets
+        text = " ".join(i for i in df["text"])
+
+        # Loading the Ukrainian flag image as a mask
+        mask = Image.open("drapeau-ukraine.png").convert("RGBA")
+        white_bg = Image.new("RGBA", mask.size, "WHITE")
+        white_bg.paste(mask, (0, 0), mask)
+        white_bg.convert("RGB")
+        ukraine_shape_mask = np.array(white_bg)
+
+        # Colors of the wordcloud
+        custom_colors = ["royalblue", "gold"]
+        custom_cmap = ListedColormap(custom_colors)
+
+        # Creating wordcloud
+        wordcloud = WordCloud(
+            stopwords=stopwords,
+            height=400,
+            width=800,
+            mask=ukraine_shape_mask,
+            normalize_plurals=True,
+            colormap=custom_cmap,
+            background_color="white",
+        ).generate(text)
+
+        # Creating a BytesIO buffer to save the WordCloud image
+        img_buffer = BytesIO()
+
+        # Saving the WordCloud image to the buffer in PNG format
+        wordcloud.to_image().save(img_buffer, format="PNG")
+
+        # Getting the binary data of the PNG image
+        img_binary = img_buffer.getvalue()
+
+        return img_binary
+
 
     def get_polarity_cloropleth_data(self, date):
         """
