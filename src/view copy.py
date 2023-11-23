79db71de2e021e43ed940ae3cffb6d5a0daf3de8
Lefeuve Vincent <vincent.lefeuve@student-cs.fr>
2023-11-23 09:36:29 +0100

    Continued refactoring

--- src/view copy.py
+++ src/view copy.py
@@ -35,68 +35,68 @@ class View:
         self.setup_layout()
         self.setup_callbacks()
 
-    def create_layout(self) -> Html.Div:
+    def setup_layout(self):
         return html.Div([
             create_header(), # Header of the website
             html.Div([ # Body
                 html.Div([
                     create_date_dropdown(),
                     html.Div([
-                        create_option_dropdown('map-dropdown'),
-                        create_choropleth_map()
+                        DropdownCreator().create_option_dropdown('map-dropdown'),
+                        Visualizor().create_choropleth_map(self.controller.get_choropleth_data('02/04', False))
                     ]),
                     html.Div([
-                        create_option_dropdown('bar-chart-dropdown'),
-                        create_bar_chart()
+                        DropdownCreator().create_option_dropdown('bar-chart-dropdown'),
+                        Visualizor().create_bar_chart()
                     ]),
                     html.Div([
-                        create_option_dropdown('wordcloud-dropdown'),
-                        create_wordcloud()
+                        DropdownCreator().create_option_dropdown('wordcloud-dropdown'),
+                        Visualizor().create_wordcloud()
                     ]),
                 ])
                 html.Div([
-                        create_country_dropdown(),
-                        create_line_chart()
+                        DropdownCreator().create_country_dropdown(),
+                        Visualizor().create_line_chart()
                 ])
             ], style=body_style)
         ])
     
+    def setup_callbacks(self):
+        @app.callback(
+        Output('choropleth-map', 'figure'),
+        [Input('date-dropdown', 'value'), Input('choropleth-option-dropdown', 'value')]
+        )
+        def update_choropleth(selected_date, selected_option):
+            data = model.get_choropleth_data(selected_date, selected_option)
+            figure = Visualizor().create_choropleth_figure(data)  
+            return figure
 
-    @app.callback(
-    Output('choropleth-map', 'figure'),
-    [Input('date-dropdown', 'value'), Input('choropleth-option-dropdown', 'value')]
-    )
-    def update_choropleth(selected_date, selected_option):
-        data = model.get_choropleth_data(selected_date, selected_option)
-        figure = Visualizor().create_choropleth_figure(data)  
-        return figure
-
-    @app.callback(
-    Output('bar-chart', 'figure'),
-    [Input('date-dropdown', 'value'), Input('bar-chart-dropdown', 'value')]
-    )
-    def update_bar_chart(selected_date, selected_option):
-        data = generate_bar_chart_data(selected_date, selected_option)
-        figure = Visualizor().create_bar_chart_figure(data)
-        return figure
+        @app.callback(
+        Output('bar-chart', 'figure'),
+        [Input('date-dropdown', 'value'), Input('bar-chart-dropdown', 'value')]
+        )
+        def update_bar_chart(selected_date, selected_option):
+            data = generate_bar_chart_data(selected_date, selected_option)
+            figure = Visualizor().create_bar_chart_figure(data)
+            return figure
     
 
-    @app.callback(
-    Output('wordcloud', 'figure'),
-    [Input('date-dropdown', 'value'), Input('wordcloud-dropdown', 'value')]
-    )
-    def update_wordcloud(selected_date, selected_option):
-        data = generate_wordcloud_data(selected_date, selected_option)
-        figure = Visualizor().create_wordcloud_figure(data)
-        return figure
-    
-    @app.callback(
-    Output('line-chart', 'figure'),
-    [Input('country-dropdown', 'value')]
-    )
-    def update_line_chart(selected_country):
-        data = generate_line_chart_data(selected_country)
-        figure = Visualizor().create_line_chart_figure(data)
-        return figure
+        @app.callback(
+        Output('wordcloud', 'figure'),
+        [Input('date-dropdown', 'value'), Input('wordcloud-dropdown', 'value')]
+        )
+        def update_wordcloud(selected_date, selected_option):
+            data = generate_wordcloud_data(selected_date, selected_option)
+            figure = Visualizor().create_wordcloud_figure(data)
+            return figure
+        
+        @app.callback(
+        Output('line-chart', 'figure'),
+        [Input('country-dropdown', 'value')]
+        )
+        def update_line_chart(selected_country):
+            data = generate_line_chart_data(selected_country)
+            figure = Visualizor().create_line_chart_figure(data)
+            return figure
 
 
